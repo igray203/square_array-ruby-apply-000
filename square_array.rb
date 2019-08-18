7b5db62d6420a91a4dd2e950b8ab82e493efc487
ian gray <igrayscale@gmail.com>
2019-08-18 21:19:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,13 +2,14 @@ ants = [1,2,3]
 
 def square_array(array)
   # your code here
-
+<<<<<<< HEAD
   counter = 0
   array.each do |number|
     array[counter] = number**2
       counter += 1
-#  array.each do |number|
-#    puts number**2
-
+=======
+  array.each do |number|
+    puts number**2
+>>>>>>> e5140ae3e764520032cd852b8848969c115cad58
   end
 end
